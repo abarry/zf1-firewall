bb87be46e6e85a30fbd669dba8fb7767da75fdd9
Devlopnet <hi.pascalous@yahoo.fr>
2012-08-15 23:44:55 +0200

    Ajout de configurations Linux

---
+++ Linux/nginx/setup.sh
@@ -0,0 +1,1 @@
+openssl req -x509 -nodes -newkey rsa:1024 -keyout /etc/ssl/private/ssl-nginx.key -out /etc/ssl/certs/ssl-nginx.pem
---
+++ Linux/php5-fpm/setup.sh
@@ -0,0 +1,2 @@
+sudo apt-get install php5-fpm php-pear #php5-dev
+sudo pecl install apc
