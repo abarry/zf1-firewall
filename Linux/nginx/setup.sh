40f157a2cd8a59fac9f3dd162b267288c3ed36b5
Devlopnet <hi.pascalous@yahoo.fr>
2012-10-02 00:16:51 +0200

    Move to linux workstation

--- Linux/nginx/setup.sh
+++
@@ -1,1 +0,0 @@
-openssl req -x509 -nodes -newkey rsa:1024 -keyout /etc/ssl/private/ssl-nginx.key -out /etc/ssl/certs/ssl-nginx.pem
--- Linux/php5-fpm/setup.sh
+++
@@ -1,2 +0,0 @@
-sudo apt-get install php5-fpm php-pear #php5-dev
-sudo pecl install apc
